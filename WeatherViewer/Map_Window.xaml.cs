e785e95adcb78e350965159b228e46321294f3d4
Nicolas Lépy <33573107+nicolasLepy@users.noreply.github.com>
2020-07-03 18:12:05 +0200

    Removed unused using

--- WeatherViewer/Map_Window.xaml.cs
+++ WeatherViewer/Map_Window.xaml.cs
@@ -2,20 +2,15 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using WeatherPrediction;
-using Matrix = WeatherPrediction.Matrix;
 
 namespace WeatherViewer
 {
